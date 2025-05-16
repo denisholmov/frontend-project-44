2963ff7687e6aabfc2c66ae08bfa5dd3a0870d72
Denis <denisskholmov@gmail.com>
2025-05-16 16:12:46 +0300

    подправил по стандарту sonar qube

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,88 +1,92 @@
 export const calcNums = (num1, num2, operator) => {
   switch (operator) {
     case '+':
-      return num1 + num2
+      return num1 + num2;
 
     case '-':
-      return num1 - num2
+      return num1 - num2;
 
     case '*':
-      return num1 * num2
+      return num1 * num2;
   }
-}
+};
 
 export const generationMathOperator = () => {
-  const num = Math.floor(Math.random() * 3)
+  // nosonar - not used for security purposes
+  const num = Math.floor(Math.random() * 3);
 
   switch (num) {
     case 0:
-      return '+'
+      return '+';
 
     case 1:
-      return '-'
+      return '-';
 
     default:
-      return '*'
+      return '*';
   }
-}
+};
 
-export const isEven = (num) => {
+export const isEven = num => {
   if (num % 2 === 0) {
-    return true
+    return true;
   }
 
-  return false
-}
+  return false;
+};
 
 export const gcd = (a, b) => {
-  a = Math.abs(a)
-  b = Math.abs(b)
+  a = Math.abs(a);
+  b = Math.abs(b);
 
   while (b !== 0) {
-    const temp = b
-    b = a % b
-    a = temp
+    const temp = b;
+    b = a % b;
+    a = temp;
   }
 
-  return a
-}
+  return a;
+};
 
 export const generationNumber = () => {
-  return Math.floor(Math.random() * 101)
-}
+  // nosonar - not used for security purposes
+  return Math.floor(Math.random() * 101);
+};
 
 export const generationArithmeticProgression = () => {
-  const lengthProgression = Math.floor(Math.random() * (20 - 5 + 1)) + 5
-  const stepProgression = Math.floor(Math.random() * (10 - 1 + 1)) + 1
-  const start = Math.floor(Math.random() * 100)
+  // nosonar - not used for security purposes
+  const lengthProgression = Math.floor(Math.random() * (20 - 5 + 1)) + 5;
+  const stepProgression = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+  const start = Math.floor(Math.random() * 100);
 
-  let current = start
+  let current = start;
 
-  const newArr = []
+  const newArr = [];
 
   for (let i = 0; i < lengthProgression; i += 1) {
-    newArr.push(current)
-    current += stepProgression
+    newArr.push(current);
+    current += stepProgression;
   }
 
-  return newArr
-}
+  return newArr;
+};
 
-export const replaceRandomWithDots = (arr) => {
-  const positionPoints = Math.floor(Math.random() * arr.length)
-  const newArr = arr.map((item, index) => (index !== positionPoints ? item : '..'))
+export const replaceRandomWithDots = arr => {
+  // nosonar - not used for security purposes
+  const positionPoints = Math.floor(Math.random() * arr.length);
+  const newArr = arr.map((item, index) => (index !== positionPoints ? item : '..'));
 
-  return [newArr, arr[positionPoints]]
-}
+  return [newArr, arr[positionPoints]];
+};
 
-export const isPrime = (num) => {
-  if (num <= 1) return false // 0 и 1 — не простые
-  if (num === 2) return true // 2 — простое
+export const isPrime = num => {
+  if (num <= 1) return false; // 0 и 1 — не простые
+  if (num === 2) return true; // 2 — простое
 
   // Проверяем делители от 2 до корня из числа
   for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) return false
+    if (num % i === 0) return false;
   }
 
-  return true
-}
+  return true;
+};
