d2b6dc9a047cc35036512202a64ed1fac93277e0
Denis <denisskholmov@gmail.com>
2025-05-16 15:23:24 +0300

    Исправил brain-prime

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,27 +1,14 @@
 #!/usr/bin/env node
 
-import welcomeUser from '../src/cli.js';
+import runGame from '../src/games/game.js';
 import { generationNumber, isPrime } from '../src/helpers/helpers.js';
-import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
+const generatePrimeQuestion = () => {
+  const num = generationNumber();
+  const question = num;
+  const correctAnswer = isPrime(num) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
 
-const name = welcomeUser();
-
-console.log(`Hello, ${name}!`);
-
-console.log(`Answer "yes" if given number is prime. Otherwise answer "no".`);
-
-const num = generationNumber();
-const correctAnswer = isPrime(num) ? 'yes' : 'no';
-
-const question = num;
-console.log(`Question: ${question}`);
-
-const userAnswer = readlineSync.question('Your answer: ');
-
-if (String(userAnswer) === String(correctAnswer)) {
-  console.log('Correct!');
-} else {
-  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-}
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+runGame(description, generatePrimeQuestion);
